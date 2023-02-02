4cc0fe33fcff26d3d2db0b9773cc91f1b5fd5ac6
Лапин Андрей (Lapin Andrey) <nethack@mail.ru>
2023-02-02 16:43:30 +0400

    chore: add typescript

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var express = require('express');
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    console.log(req);
+    res.send('Hello World!');
+});
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
+});
