52ea232cc08d32fb9e5c994df21898923b1eafd2
Лапин Андрей (Lapin Andrey) <nethack@mail.ru>
2023-02-02 18:32:29 +0400

    chore: typed enterpoint/express

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
+//https://dev.to/macmacky/get-better-with-typescript-using-express-3ik6 this is good helper
+//app.get<Params,ResBody,ReqBody,ReqQuery,Locals>
+//Params - id in url
+//ResBody - response json
+//ReqBody - request body
+//ReqQuery - request query params
+app.get<
+{},
+{ data: string[], message: string },
+{},
+{ page: number, limit: number, breed: 'labrador' | 'german shepherd' | 'golden retriever' }>
+('/', (req, res) => {
+  res.send({data: ['доска не доска. лист не лист'], message: 'Start?'})
 })
 
 app.listen(port, () => {
