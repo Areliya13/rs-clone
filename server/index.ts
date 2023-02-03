b0067eca597a5597a548f96220a113b2a70340cf
Лапин Андрей (Lapin Andrey) <nethack@mail.ru>
2023-02-03 13:34:31 +0400

    feat: add first endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose, {connect, Schema, model, isValidObjectId, modelNames } from 'mongoose';
+
 const app = express()
 const port = 3000
 
@@ -8,6 +10,48 @@ const port = 3000
 //ResBody - response json
 //ReqBody - request body
 //ReqQuery - request query params
+
+
+interface IUser {
+  name: string;
+  email: string;
+  avatar?: string;
+  arr: string[];
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true  },
+  email: { type: String, required: true },
+  avatar: String,
+  arr: Array<String>
+});
+
+const User = model<IUser>('User', userSchema);  //это функция конструктор (класс)
+
+main().catch(err => console.log(err));
+async function main() {
+  await connect('mongodb+srv://admin:v1VBsDSCDlkzGIKR@trello.id2jf8j.mongodb.net/trello?retryWrites=true&w=majority');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  const user = new User({
+    name: 'Bill22',
+    email: 'bill@initech.com',
+    avatar: 'https://i.imgur.com/dM7Thhn.png',
+    arr: ['s', 'd']
+  });
+  const x = await User.findById('63dc0e424d7c4ebacb50393c');
+
+  console.log(user.email, x);
+  
+}
+
+async function zz() {
+  const d = await isValidObjectId('63dsfe716068f134993f3117s')
+  const z = await modelNames()
+  console.log('d', d, z);
+}
+  zz()
+
+
 app.get<
 {},
 { data: string[], message: string },
