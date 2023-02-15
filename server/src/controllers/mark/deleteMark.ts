8fd05290103953b02a1a0ef8273e6bdaaeee8265
Lapin Andrey <nethack@mail.ru>
2023-02-15 19:50:50 +0300

    fix: add parentId2 to deleteMarkController

--- server/src/controllers/mark/deleteMark.ts
+++ server/src/controllers/mark/deleteMark.ts
@@ -4,8 +4,9 @@ import { deleteMarkService } from "../../services/mark/deleteMark"
 export const deleteMarkController = async (req: Request, res: Response, next: NextFunction) => {
     try{
       const boardId = req.body.parentId
+      const itemId = req.body.parentId2
       const markId = req.params.id
-      const response = await deleteMarkService(markId, boardId)
+      const response = await deleteMarkService(markId, boardId, itemId)
       res.status(200).send(response)
     }
     catch(e) {
