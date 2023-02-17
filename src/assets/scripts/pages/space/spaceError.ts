40dad7e89bf45bdbfffe6a7b619bc939eb212a08
Areliya13 <sharipovatamara@gmail.com>
2023-02-17 04:24:02 +0300

    feat: add info from store

--- src/assets/scripts/pages/space/spaceError.ts
+++ src/assets/scripts/pages/space/spaceError.ts
@@ -3,7 +3,7 @@ import { createHtmlElement } from '../../helpers/other';
 class SpaceError {
   renderErrorMessage(): HTMLDivElement {
     const container = createHtmlElement('div');
-    const message = createHtmlElement('h1', { textContent: 'There is no such a page!' });
+    const message = createHtmlElement('h1', { textContent: 'Что-то пошло не так!' });
     container.append(message);
     return container;
   }
