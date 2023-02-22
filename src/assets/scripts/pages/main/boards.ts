279b5e375f8ab17ec4491c5c9f3a41a56922fa57
Lapin Andrey <nethack@mail.ru>
2023-02-22 23:01:33 +0300

    feat: add lists

--- src/assets/scripts/pages/main/boards.ts
+++ src/assets/scripts/pages/main/boards.ts
@@ -4,8 +4,10 @@ import clockIcon from '../../../images/clock-icon.inl.svg';
 import star from '../../../images/star.inl.svg';
 import { BigBoardList } from '../../components/BigBoardList/BigBoardList';
 import observer from '../../store/observer';
-import { EventName, IPartialUser, IStore } from '../../store/types';
+import { EventName, IPartialUser, IStore, IWork } from '../../store/types';
 import { store } from '../../store/store';
+import { getAllUserBoards } from '../../helpers/utils/getAllUserBoard';
+import { WorkSpaceBigBoardList } from '../../components/WorkSpaceBigBoardList/WorkSpaceBigBoardList';
 
 export class Boards extends Page{
   constructor(id: string) {
@@ -34,11 +36,21 @@ export class Boards extends Page{
     const recentlyIcon = createHtmlElement('div', {className: 'boardsRecentlyIcon', innerHTML: clockIcon})
     const recentlyText = createHtmlElement('h3', { className: 'boardsRecentlyText' , textContent: 'Недавно просмотренные'})
     recentlyHead.append(recentlyIcon, recentlyText);
-    recentlyViewed.append(recentlyHead);
+
+    let recently = getAllUserBoards(store.user)
+    if (!recently) recently = []
+    const recentlyBoardsList = new BigBoardList(recently, 'big-board-list-recently').getList()
+    recentlyViewed.append(recentlyHead, recentlyBoardsList);
 
     const yourWorkspaces = createHtmlElement('section', { className: 'boardsWorkspaces' });
     const yourWorkspacesHead = createHtmlElement('h3', { className: 'boardsWorkspacesHead', textContent: 'Ваши рабочие пространства' })
+    const yourWorkspacesList = createHtmlElement('div', { className: 'boardsWorkspacesList' });
     yourWorkspaces.append(yourWorkspacesHead);
+
+    let workspaces = store.user?.workSpace
+    if (!workspaces) workspaces = []
+    this.renderWorkSpaceLists(workspaces, yourWorkspacesList)
+    yourWorkspaces.append(yourWorkspacesList)
 
     const closedWorkspacesButton = createHtmlElement('button', { className: 'closedWorkspacesButton', textContent: 'Посмотреть закрытые доски'})
 
@@ -57,6 +69,8 @@ export class Boards extends Page{
 
   update(store: IPartialUser) {
     this.renderFavoriteBoard(store)
+    this.renderRecentlyBoard(store)
+    this.reRenderWorkSpaceLists(store)
   }
 
   renderFavoriteBoard(data: IPartialUser) {
@@ -76,5 +90,48 @@ export class Boards extends Page{
     boardsFavorite.append(newFavoriteList)
   }
 
+  renderRecentlyBoard(data: IPartialUser) {
+    const recentlyList = document.querySelector('.big-board-list-recently')
+    const boardsRecently = document.querySelector('.boardsRecently')
+
+    if (!recentlyList) return
+    recentlyList.remove()
+
+    if (!boardsRecently) return
+
+    let recentlyListInStore = getAllUserBoards(data)
+    if (!recentlyListInStore) recentlyListInStore = []
+    console.log(recentlyListInStore)
+    const newRecentlyList = new BigBoardList(recentlyListInStore, 'big-board-list-recently').getList()
+
+    boardsRecently.append(newRecentlyList)
+  }
+
+  renderWorkSpaceLists(workSpaces: IWork[], container: Element) {
+    workSpaces.forEach((workspace) => {
+      const header = createHtmlElement('div', {
+        className: `work-space-big-board-list-header`
+    })
+    const workspaceName = createHtmlElement('span', {
+        className: 'work-space-big-board-list-name', 
+        textContent: `${workspace.title}`
+    })
+    const workspaceIcon = createHtmlElement('div', {
+        className: 'work-space-big-board-list-icon', 
+        textContent: `${workspaceName.textContent[0].toUpperCase()}`
+    });
+    header.append(workspaceIcon, workspaceName)
+      const workSpacesList = new WorkSpaceBigBoardList(workspace, 'work-space-big-board-list-da').getList()
+      container.append(header, workSpacesList)
+    })
+  }
+
+  reRenderWorkSpaceLists(data: IPartialUser) {
+    const workSpaceList = document.querySelector('.boardsWorkspacesList')
+    if (!workSpaceList) return
+    workSpaceList.innerHTML = ''
+    const workSpaces = data.workSpace
+    this.renderWorkSpaceLists(workSpaces, workSpaceList)
+  }
   
 }
